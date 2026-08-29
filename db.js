3282b0f73a58caf0c87bc2506cb36c2a8f5f4d90
nachofigueroa <messiiie100@gmail.com>
2026-08-28 21:27:02 -0300

    Fix syntax errors in database connection setup

--- db.js
+++ db.js
@@ -1,17 +1,19 @@
-const mysql = requiere("mysql2");
-const conexion = mysql.createconection({
-    host:"10.0.9.40",
-    user : "root",
+const mysql = require("mysql2");
+
+const conexion = mysql.createConnection({
+    host: "10.0.9.40",
+    user: "root",
     password: "1234",
     database: "Grupo7_Planificaciones",
-    port: 3306,
+    port: 3306
 });
-    conexion.connect((err)=>{
-        if (err){
-            console.error("error de conexion:",err);
-            return;
-        }
-        console.log("conectado a la base de datos");
+
+conexion.connect((err) => {
+    if (err) {
+        console.error("error de conexion:", err);
+        return;
     }
-    );
-     module.exports=conexion;
+    console.log("conectado a la base de datos");
+});
+
+module.exports = conexion;
